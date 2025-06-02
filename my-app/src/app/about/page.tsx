773b2b72959f875c85a7fb84413233f47f1bd720
KouichiOtobe <otobe@s-gardening.jp>
2025-06-02 16:57:11 +0900

    🎨 Aboutページの追可

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import { Learn } from "../Components/otobeTest"
+
+// app/about/page.tsx
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>このサイトについて</h1>
+      <p>このページは About ページです。</p>
+      <Learn />
+
+    </div>
+  );
+}
+
+export default AboutPage;
