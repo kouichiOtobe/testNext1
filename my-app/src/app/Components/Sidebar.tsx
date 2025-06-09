b3bd1021403f16a01412e3f586ee8dea6c40b0d5
KouichiOtobe <otobe@s-gardening.jp>
2025-06-09 17:30:29 +0900

    🔖 サイドバーの階層メニュー化の一応動作版

--- my-app/src/app/Components/Sidebar.tsx
+++ my-app/src/app/Components/Sidebar.tsx
@@ -1,27 +1,34 @@
 'use client'; // App Router を使っている場合は必要（クライアントコンポーネント）
 import React from 'react'
+import { usePathname } from "next/navigation";
 // import { useEffect, useState } from 'react';
 import { SideBarMenu, MenuItem } from './SideBarData'
 import { SidebarIcon } from './SidebarIcon';
 import Link from 'next/link';
 import clsx from 'clsx';
-import { usePathname } from 'next/navigation';
 import { useState } from "react";
 
 function SidebarItem({ item }: { item: MenuItem }) {
   const [open, setOpen] = useState(false);
   const hasChildren = item.children && item.children.length > 0;
 
+  const pathname = usePathname();
+　const isActive = pathname === item.path;
+
   return (
     <div className="ml-2">
   
       <div
-        className="flex items-center gap-2 px-4 py-2 hover:bg-blue-600"
+        className=""
         onClick={() => hasChildren && setOpen(!open)}
       >
-        <div id="icon">{item.icon}</div>
-        {hasChildren ? (open ? "▼ " : "▶ ") : " "}
-        <Link href={item.path}>{item.title}</Link>
+        <Link href={item.path}
+          className={clsx("flex items-center gap-2 px-4 py-2 rounded transition-colors",
+             isActive ? "bg-white text-black" : "text-white hover:bg-blue-600")}>
+          <div id="icon">{item.icon}</div>
+            {hasChildren ? (open ? "▼ " : "▶ ") : " "}
+          {item.title}
+        </Link>
       </div>
       {open && hasChildren && (
         <div className="ml-4">
